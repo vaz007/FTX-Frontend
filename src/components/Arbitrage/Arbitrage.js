2c983ae624556798cfd1b104c83f70494ab4e354
koustubhv <koustubhv@gmail.com>
2021-11-14 07:44:14 +0800

    Changes in arbitrage.js

--- src/components/Arbitrage/Arbitrage.js
+++ src/components/Arbitrage/Arbitrage.js
@@ -14,6 +14,12 @@ const Arbitrage = () => {
             headerName: "bpsBasis", field: "bpsBasis", sortable: true, filter: true
         },
         {
+            headerName: "absBasis", field: "absBasis", sortable: true, filter: true
+        },
+        {
+            headerName: "spotPrice", field: "spotPrice", sortable: true, filter: true
+        },
+        {
             headerName: "futurePrice", field: "futurePrice", sortable: true, filter: true
         },
         {
@@ -24,12 +30,6 @@ const Arbitrage = () => {
         },
         {
             headerName: "midPrice", field: "midPrice", sortable: true, filter: true
-        },
-        {
-            headerName: "spotPrice", field: "spotPrice", sortable: true, filter: true
-        },
-        {
-            headerName: "absBasis", field: "absBasis", sortable: true, filter: true
         },
     ]
 
@@ -57,15 +57,15 @@ const Arbitrage = () => {
                 res.data.map(item => {
                     result.push({
                         symbol: item.data[0].symbol,
-                        bpsBasis: item.data[0].absPrice.toFixed(4),
-                        futurePrice: item.data[0].futurePrice.toFixed(4),
-                        bidPrice: item.data[0].bidPrice.toFixed(4),
-                        askPrice: item.data[0].askPrice.toFixed(4),
-                        askSize: item.data[0].askSize.toFixed(4),
-                        bidSize: item.data[0].bidSize.toFixed(4),
-                        midPrice: item.data[0].midPrice.toFixed(4),
-                        spotPrice: item.data[0].spotPrice.toFixed(4),
-                        absBasis : (item.data[0].futurePrice - item.data[0].spotPrice).toFixed(4),
+                        bpsBasis: item.data[0].absPrice.toFixed(3),
+                        futurePrice: item.data[0].futurePrice.toFixed(3),
+                        bidPrice: item.data[0].bidPrice.toFixed(3),
+                        askPrice: item.data[0].askPrice.toFixed(3),
+                        askSize: item.data[0].askSize.toFixed(3),
+                        bidSize: item.data[0].bidSize.toFixed(3),
+                        midPrice: item.data[0].midPrice.toFixed(3),
+                        spotPrice: item.data[0].spotPrice.toFixed(3),
+                        absBasis : (item.data[0].futurePrice - item.data[0].spotPrice).toFixed(3),
                     })
                 })
                 setRowData([...result])
