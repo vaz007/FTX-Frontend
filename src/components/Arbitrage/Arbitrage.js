9e09aed8357ddb7082a60828993b1cfb658105c7
koustubhv <koustubhv@gmail.com>
2021-11-14 07:40:57 +0800

    Changes in arbitrage.js

--- src/components/Arbitrage/Arbitrage.js
+++ src/components/Arbitrage/Arbitrage.js
@@ -11,7 +11,7 @@ const Arbitrage = () => {
             headerName: "symbol", field: "symbol", sortable: true, filter: true
         },
         {
-            headerName: "absPrice", field: "absPrice", sortable: true, filter: true
+            headerName: "bpsBasis", field: "bpsBasis", sortable: true, filter: true
         },
         {
             headerName: "futurePrice", field: "futurePrice", sortable: true, filter: true
@@ -28,7 +28,9 @@ const Arbitrage = () => {
         {
             headerName: "spotPrice", field: "spotPrice", sortable: true, filter: true
         },
-
+        {
+            headerName: "absBasis", field: "absBasis", sortable: true, filter: true
+        },
     ]
 
     useEffect(() => {
@@ -55,14 +57,15 @@ const Arbitrage = () => {
                 res.data.map(item => {
                     result.push({
                         symbol: item.data[0].symbol,
-                        absPrice: Math.round((item.data[0].absPrice + Number.EPSILON) * 100) / 100,
-                        futurePrice: Math.round((item.data[0].futurePrice + Number.EPSILON) * 100) / 100,
-                        bidPrice: Math.round((item.data[0].bidPrice + Number.EPSILON) * 100) / 100,
-                        askPrice: Math.round((item.data[0].askPrice + Number.EPSILON) * 100) / 100,
-                        askSize: Math.round((item.data[0].askSize + Number.EPSILON) * 100) / 100,
-                        bidSize: Math.round((item.data[0].bidSize + Number.EPSILON) * 100) / 100,
-                        midPrice: Math.round((item.data[0].midPrice + Number.EPSILON) * 100) / 100,
-                        spotPrice: Math.round((item.data[0].spotPrice + Number.EPSILON) * 100) / 100,
+                        bpsBasis: item.data[0].absPrice.toFixed(4),
+                        futurePrice: item.data[0].futurePrice.toFixed(4),
+                        bidPrice: item.data[0].bidPrice.toFixed(4),
+                        askPrice: item.data[0].askPrice.toFixed(4),
+                        askSize: item.data[0].askSize.toFixed(4),
+                        bidSize: item.data[0].bidSize.toFixed(4),
+                        midPrice: item.data[0].midPrice.toFixed(4),
+                        spotPrice: item.data[0].spotPrice.toFixed(4),
+                        absBasis : (item.data[0].futurePrice - item.data[0].spotPrice).toFixed(4),
                     })
                 })
                 setRowData([...result])
